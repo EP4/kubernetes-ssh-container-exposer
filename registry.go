0957d62db54eb3cceb483f7cb6525818e5cd32ef
Samuel Hautcoeur <samuel.hautcoeur@ep4.com>
2019-02-09 03:14:35 +0000

    Support for multiple public keys

--- registry.go
+++ registry.go
@@ -167,7 +167,7 @@ func (r *Registry) RegisterUpstream(upstream *Upstream) (*Upstream, error) {
 
 	DownstreamPublicKeys := strings.Split(upstream.DownstreamPublicKey, "\n")
 	for i := range DownstreamPublicKeys {
-		logger.Info(fmt.Println(DownstreamPublicKeys[i]))
+		logger.Info(fmt.Println(string(DownstreamPublicKeys[i])))
 		pub := crud.NewPublicKeys(r.database)
 		if rec, err := pub.GetFirstByName(upstream.Name); err == nil {
 			if rec != nil {
