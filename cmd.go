564cfcfc45fa883b788c834b8b51c6cc4a0c2861
Samuel Hautcoeur <samuel.hautcoeur@ep4.com>
2019-02-10 05:28:33 +0000

    Support for multiple public keys

--- cmd.go
+++ cmd.go
@@ -75,7 +75,7 @@ func getKeys(client kubernetes.Interface, namespace string, name string) (Keys, 
 		return Keys{}, err
 	}
 	var DownstreamPublicKeys []string
-	SecretDownstreamPublicKeys := bytes.Split(bytes.NewBuffer(secret.Data["downstream_id_rsa.pub"]), "\n")
+	SecretDownstreamPublicKeys := bytes.Split(secret.Data["downstream_id_rsa.pub"], "\n")
 	for _, DownstreamPublicKey := range SecretDownstreamPublicKeys {
 		ByteDownstreamPublicKey, _, _, _, err := ssh.ParseAuthorizedKey(DownstreamPublicKey)
 		if err != nil {
